df10e634baba3ee5231459491228fef499bbceae
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-06-22 23:58:22 +0530

    Hello world quick before time runs out

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,4 +12,8 @@ public func routes(_ router: Router) throws {
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.get("hello"){req in
+        return "Hello World"
+        
+    }
 }
